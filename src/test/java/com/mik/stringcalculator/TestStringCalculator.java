c5804dc360eb703a4e94a18b703de183320b28c8
monjurul003 <monjurul.khan@gmail.com>
2020-09-29 22:46:04 -0700

    String calculator- test with single number added.

--- src/test/java/com/mik/stringcalculator/TestStringCalculator.java
+++ src/test/java/com/mik/stringcalculator/TestStringCalculator.java
@@ -1,15 +1,32 @@
 package com.mik.stringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestStringCalculator {
+    StringCalculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new StringCalculator();
+    }
+
     @Test
     @DisplayName("Test with empty string")
     public void testWithEmptyString() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(0, calculator.add(""));
+        int expected = 0;
+        int actual = calculator.add("");
+        assertEquals(expected, actual, () -> "should return " + expected + " but returned " + actual);
+    }
+
+    @Test
+    @DisplayName("Test with single number in the string")
+    public void testWithOneNumber() {
+        int expected = 1;
+        int actual = calculator.add("1");
+        assertEquals(expected, actual, () -> "should return " + expected + " but returned " + actual);
     }
 }
